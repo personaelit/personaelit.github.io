e638cc2d96fbcaa094dae6a4c70e06c77377642f
Jim Smits <jim.smits@stayonline.com>
2024-06-15 01:54:40 -0400

    deleted files

--- projects/TaskManagement/README.md
+++
@@ -1,1 +0,0 @@
-The project will be built on the MERN stack using mongoose for modeling and Mocha Chai for testing.
--- projects/_waste basket/units/tagManager.js
+++
@@ -1,8 +0,0 @@
-const addTag = (tags, newTag) => [...tags, newTag];
-const removeTag = (tags, tagToRemove) => tags.filter(tag => tag !== tagToRemove);
-const getAllTags = (tags) => [...tags];
-const tagExists = (tags, tagToCheck) => tags.includes(tagToCheck);
-
-module.exports = { addTag, removeTag, getAllTags, tagExists };
-
-
