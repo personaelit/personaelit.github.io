c00060fd712996db3a05d6b1964e1575fed2fad7
Jim Smits <jim.smits@stayonline.com>
2024-06-24 12:04:03 -0400

    add touch support to kanban

--- portfolio/Kanban/Kanban.js
+++ portfolio/Kanban/Kanban.js
@@ -19,35 +19,42 @@ window.addEventListener('DOMContentLoaded', (event) => {
     lists.forEach(function (quad) {
         quad.addEventListener('dragover', handleDragover);
         quad.addEventListener('drop', handleDrop);
-    })
+        quad.addEventListener('touchmove', handleTouchMove);
+        quad.addEventListener('touchend', handleTouchEnd);
+    });
     
     const plusses = document.querySelectorAll('.plus');
     plusses.forEach(function (plus) {
         plus.addEventListener('click', handlePlusClick);
-    })
+    });
 
-    //NOTE: reapply event listeners to tasks that were retrieved from localStorage.
+    // Reapply event listeners to tasks that were retrieved from localStorage.
     const tasks = document.querySelectorAll('.task');
     tasks.forEach(function (task) {
         task.addEventListener('dragover', handleDragover);
         task.addEventListener('dragstart', handleDragStart);
         task.addEventListener('dragend', handleDragEnd);
         task.addEventListener('keyup', handleKeyup);
-    })
+        task.addEventListener('touchstart', handleTouchStart);
+        task.addEventListener('touchmove', handleTouchMove);
+        task.addEventListener('touchend', handleTouchEnd);
+    });
 
-    //NOTE: reapply event listeners to x that were retrieved from localStorage.
+    // Reapply event listeners to x that were retrieved from localStorage.
     const exes = document.querySelectorAll('.x');
     exes.forEach(function (x) {
         x.addEventListener('click', handleXClick);
-    })
+    });
 
     function handlePlusClick(e) {
-        
         const task = document.createElement('div');
         task.setAttribute('class', 'task');
         task.addEventListener('dragover', handleDragover);
         task.addEventListener('dragstart', handleDragStart);
         task.addEventListener('dragend', handleDragEnd);
+        task.addEventListener('touchstart', handleTouchStart);
+        task.addEventListener('touchmove', handleTouchMove);
+        task.addEventListener('touchend', handleTouchEnd);
         task.draggable = true;        
 
         const taskInput = document.createElement('div');
@@ -56,6 +63,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
         taskInput.setAttribute('class', 'taskInput');
         
         const x = document.createElement('div');
+        x.contentEditable = false;
         x.setAttribute('class', 'x');
         x.addEventListener('click', handleXClick);
         x.innerText = "X";
@@ -91,12 +99,32 @@ window.addEventListener('DOMContentLoaded', (event) => {
         e.preventDefault();
         if (e.target === draggedTask) return false;
         draggedTask.parentNode.removeChild(draggedTask);
-        this.appendChild(draggedTask)
+        this.appendChild(draggedTask);
         storeBody();
     }
 
     function handleDragover(e) {
         e.preventDefault();
+    }
+
+    function handleTouchStart(e) {
+        if (e.target.nodeType === Node.TEXT_NODE) {
+            return false;
+        }
+        draggedTask = e.target;
+    }
+    
+    function handleTouchMove(e) {
+        e.preventDefault();
+        const touchLocation = e.targetTouches[0];
+        const element = document.elementFromPoint(touchLocation.clientX, touchLocation.clientY);
+        if (element && element.classList.contains('list')) {
+            element.appendChild(draggedTask);
+        }
+    }
+    
+    function handleTouchEnd(e) {
+        storeBody();
     }
 
     function storeBody() {
