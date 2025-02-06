63dbda86f82cef8cb6711b676237c4093ef509f7
Jim Smits <jim.smits@stayonline.com>
2025-02-06 15:41:32 -0500

    update last updated date replacement logic in set-lastupdated.ps1 for improved accuracy

--- set-lastupdated.ps1
+++ set-lastupdated.ps1
@@ -1,9 +1,9 @@
 Get-ChildItem -Recurse -Filter *.html | ForEach-Object {
     $filePath = $_.FullName
     $content = Get-Content -Path $filePath -Raw
-    if ($content -match '<span class="last-updated"></span>') {
+    if ($content -match '<span class="last-updated">.*?</span>') {
         $lastUpdated = "Last Updated: $($_.LastWriteTime.ToString('dddd, yyyy/MM/dd'))"
-        $updatedContent = $content -replace '<span class="last-updated"></span>', "<span class='last-updated'>$lastUpdated</span>"
+        $updatedContent = $content -replace '<span class="last-updated">.*?</span>', "<span class='last-updated'>$lastUpdated</span>"
         Set-Content -Path $filePath -Value $updatedContent
     }
 }
