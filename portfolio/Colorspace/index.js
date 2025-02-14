ac0595a0b0d69d665ee5e22dfcba123666193397
Jim Smits <jim.smits@stayonline.com>
2025-02-14 18:30:49 -0500

    refactor: improve hex color handling and simplify conversion functions

--- portfolio/Colorspace/index.js
+++ portfolio/Colorspace/index.js
@@ -1,68 +1,44 @@
 console.log("Hello from colorspace.");
 
 String.prototype.convertToRGB = function () {
-    //https://javascript.plainenglish.io/convert-hex-to-rgb-with-javascript-4984d16219c3
-    if (this.length != 6) {
+    if (!/^([A-Fa-f0-9]{6})$/.test(this)) {
         throw "Only six-digit hex colors are allowed.";
     }
-
-    var aRgbHex = this.match(/.{1,2}/g);
-    var aRgb = [
-        parseInt(aRgbHex[0], 16),
-        parseInt(aRgbHex[1], 16),
-        parseInt(aRgbHex[2], 16)
-    ];
-    return aRgb;
-}
+    let aRgbHex = this.match(/.{1,2}/g);
+    return aRgbHex.map(hex => parseInt(hex, 16));
+};
 
 function isHex(searchString) {
-    var noHash = searchString.replace("#", "");
-    if ((noHash.length !== 6 && noHash.length !== 3) || noHash.match(/[0-9A-Fa-f]/g).length < noHash.length) {
-        return false;
-    }
-    return true;
+    return /^#?([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(searchString);
 }
 
 function randomHex() {
-    //https://www.paulirish.com/2009/random-hex-color-code-snippets/
-    let hex = Math.floor(Math.random()*16777215).toString(16);
-    if (hex.length === 5){
-         hex = "0" + hex;
-    }
-    return '#'+ hex;
+    return "#" + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
 }
 
-    window.addEventListener('DOMContentLoaded', (event) => {
-        getRandom();
-        paint();
-    })
+window.addEventListener('DOMContentLoaded', () => {
+    getRandom();
+    paint();
+});
 
-    window.addEventListener("hashchange", (event) => {
-        paint();
-    })
+window.addEventListener("hashchange", () => {
+    paint();
+});
 
-    function paint() {
-        let hex = window.location.hash;
-
-        document.body.style.background = hex;
-        hexCode.innerText = hex;
-        let rgb = hex.replace('#', '').convertToRGB();
-        rgbCode.innerText = rgb;
-        console.log(hex);
-        console.log(rgb);
-
-        if (rgb[0] + rgb[1] + rgb[2] > 255 * 3 / 2) {
-            document.body.style.color = `rgb(${0},${0},${0})`
-        }
-        else {
-            document.body.style.color = `rgb(${255},${255},${255})`
-        }
-
-
+function paint() {
+    let hex = window.location.hash || randomHex();
+    if (!isHex(hex)) {
+        hex = "#000000"; // Default to black if invalid
     }
+    document.body.style.background = hex;
+    hexCode.innerText = hex;
+    
+    let rgb = hex.replace('#', '').convertToRGB();
+    rgbCode.innerText = `rgb(${rgb.join(", ")})`;
+    
+    document.body.style.color = (rgb.reduce((a, b) => a + b, 0) > 382) ? "black" : "white";
+}
 
-    function getRandom() {
-        window.location = window.location.pathname + randomHex();
-        
-    }
-
+function getRandom() {
+    window.location.hash = randomHex();
+}
